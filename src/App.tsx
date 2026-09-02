b9666be8e0e8ad64b25864874c14f7f522cd4d63
Arreglando los apis de firebase <jordanpmrojasbazan@gmail.com>
2026-09-02 14:52:47 -0500

    Use Sandoval login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { RegisterPage } from "./pages/RegisterPage";
-import { LoginPage } from "./pages/LoginPage";
+import { LoginPage } from "./components/login/LoginPage";
 import { TermsPage } from "./pages/Terms.Page";
 import { PrivacyPage } from "./pages/PrivacyPage";
 
